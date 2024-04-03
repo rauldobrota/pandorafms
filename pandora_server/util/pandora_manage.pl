e157d5f20248c9af9ce712166b66dfc12fa77040
Félix Suárez <felix.suarez@pandorafms.com>
2024-04-03 09:38:04 -0600

    Handle to show instructions on unknown

--- pandora_server/util/pandora_manage.pl
+++ pandora_server/util/pandora_manage.pl
@@ -7914,7 +7914,7 @@ sub pandora_manage_main ($$$) {
 					'values' => [
 						'unknown','alert_fired','alert_recovered','alert_ceased',
 						'alert_manual_validation','recon_host_detected','system',
-						'error','new_agent','going_up_warning','going_up_critical','going_down_warning',
+						'error','new_agent','going_up_warning','going_up_critical','going_down_warning','going_unknown',
 						'going_down_normal','going_down_critical','going_up_normal','configuration_change'
 					]
 				},
