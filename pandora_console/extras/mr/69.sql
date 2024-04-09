62030d67faa2562be32f5986822df736a69bc4b5
Pablo Aragon <pablo.aragon@pandorafms.com>
2024-04-09 11:20:37 +0200

    13072-Change to 69.sql

--- pandora_console/extras/mr/69.sql
+++ pandora_console/extras/mr/69.sql
@@ -1,5 +1,11 @@
 START TRANSACTION;
 
+-- Remove column id_usuario_destino from tmensajes --
+SET @exist = (SELECT count(*) FROM information_schema.columns WHERE TABLE_NAME='tmensajes' AND COLUMN_NAME='id_usuario_destino' AND table_schema = DATABASE());
+SET @sqlstmt = IF (@exist>0, 'ALTER TABLE `tmensajes` DROP COLUMN `id_usuario_destino`', 'SELECT ""');
+prepare stmt from @sqlstmt;
+execute stmt;
+
 DROP TABLE tskin;
 
 ALTER TABLE `tusuario`
--- pandora_console/extras/mr/70.sql
+++
@@ -1,8 +0,0 @@
-START TRANSACTION;
-
-SET @exist = (SELECT count(*) FROM information_schema.columns WHERE TABLE_NAME='tmensajes' AND COLUMN_NAME='id_usuario_destino' AND table_schema = DATABASE());
-SET @sqlstmt = IF (@exist>0, 'ALTER TABLE `tmensajes` DROP COLUMN `id_usuario_destino`', 'SELECT ""');
-prepare stmt from @sqlstmt;
-execute stmt;
-
-COMMIT;
